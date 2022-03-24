315ab3d88f033f3cff056127ce90b1de84cbe50d
Matthew Epps <mepps32@gmail.com>
2022-03-24 14:49:15 -0700

    remove unnecessary code, fix build

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 import { getImage } from 'gatsby-plugin-image';
-import event from '../img/bench-press.jpg';
 
 import Layout from '../components/Layout';
 import FullWidthImage from '../components/FullWidthImage';
@@ -17,34 +16,6 @@ export const IndexPageTemplate = ({
   return (
     <div>
       <FullWidthImage img={heroImage} title="EBS Performance + Fitness" />
-      {/* <section
-        className="md:px-16 md:py-16 px-4 py-8 bg-secondary"
-        style={{
-          backgroundImage: `linear-gradient(to bottom right, #6D6D6D, #d3d3d3)`,
-        }}
-      >
-        <h2 className="text-white text-3xl font-bold mb-8 text-center">
-          Upcoming Events
-        </h2>
-        <div className="flex flex-col md:flex-row md:justify-center">
-          <Link to="/bench-press">
-            <img src={event} />
-          </Link>
-          <div className="mt-6 md:mt-0 md:ml-20">
-            <h2 className="text-white text-xl font-bold mb-4">
-              Bench Press Contest
-            </h2>
-            <div className="text-white mb-1">March 5, 2022</div>
-            <div className="text-white mb-3">Registration starts at 9am</div>
-            <Link
-              to="/bench-press"
-              className="btn-primary"
-            >
-              Register Here
-            </Link>
-          </div>
-        </div>
-      </section> */}
       <WhatWeOffer />
     </div>
   );
@@ -99,25 +70,6 @@ export const pageQuery = graphql`
           childImageSharp {
             gatsbyImageData(quality: 100, layout: FULL_WIDTH)
           }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                gatsbyImageData(width: 240, quality: 64, layout: CONSTRAINED)
-              }
-            }
-            text
-          }
-          heading
-          description
         }
       }
     }
