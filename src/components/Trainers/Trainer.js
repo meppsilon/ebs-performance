c166e8ee0280166313773786cb9025dedb711696
Matthew Epps <mepps32@gmail.com>
2022-09-24 20:49:34 -0700

    chore: add hover to trainer

--- src/components/Trainers/Trainer.js
+++ src/components/Trainers/Trainer.js
@@ -5,10 +5,10 @@ const Trainer = ({ name, profile }) => (
   <Link
     to={`/trainers/${name.toLowerCase().replace(' ', '-')}`}
     key={name}
-    className="flex flex-col items-center mx-4 my-4 cursor-pointer"
+    className="flex flex-col items-center mx-4 my-4 cursor-pointer group"
   >
-    <img src={profile.publicURL} className="w-36 h-36 bg-white rounded-full" />
-    <div className="text-white">{name}</div>
+    <img src={profile.publicURL} className="w-36 h-36 bg-white rounded-full group-hover:opacity-70" />
+    <div className="text-white group-hover:text-gray-300">{name}</div>
   </Link>
 );
 
