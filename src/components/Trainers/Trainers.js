cb81235b640613979e888c05163cd0a60737a74c
Matthew Epps <mepps32@gmail.com>
2023-01-09 12:04:57 -0800

    chore: make trainers ordering consistent

--- src/components/Trainers/Trainers.js
+++ src/components/Trainers/Trainers.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import orderBy from 'lodash/orderBy';
 import Trainer from './Trainer';
 
 const Trainers = ({ trainers }) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-      {trainers.map((trainer) => (
+      {orderBy(trainers, ['node.frontmatter.name'], ['desc']).map((trainer) => (
         <Trainer
           key={trainer.node.frontmatter.name}
           {...trainer.node.frontmatter}
