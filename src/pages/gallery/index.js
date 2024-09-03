742603316e42194c724305b9852d52e574d147a9
meppsilon <matthew@hyperbolic.xyz>
2024-09-03 09:42:38 -0700

    switch out react-images and react-photo-gallery

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -1,56 +1,46 @@
 import React, { useState, useCallback } from 'react';
-import Gallery from 'react-photo-gallery';
-import Carousel, { Modal, ModalGateway } from 'react-images';
 import Layout from '../../components/Layout';
 import { range } from 'lodash';
+import Lightbox from 'yet-another-react-lightbox';
+import Inline from 'yet-another-react-lightbox/plugins/inline';
+import 'yet-another-react-lightbox/styles.css';
+import { RowsPhotoAlbum } from 'react-photo-album';
+import 'react-photo-album/rows.css';
 
-const wideImages = [2,3,4]
+// import optional lightbox plugins
+import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
+import Slideshow from 'yet-another-react-lightbox/plugins/slideshow';
+import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
+import Zoom from 'yet-another-react-lightbox/plugins/zoom';
+import 'yet-another-react-lightbox/plugins/thumbnails.css';
 
-const images = range(1, 16).map((x) => ({
-  src: `/img/carousel-${x}.jpg`,
-  ...(wideImages.includes(x) ? {
-    width: 4,
-    height: 3,
-  } : {
-    width: 3,
-    height: 4,
-  })
-}));
+import photos from '../../utils/photos';
 
 const PhotoGallery = () => {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(-1);
 
-  const openLightbox = useCallback((_, { index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
+  const updateIndex = ({ index }) => setCurrentImage(index);
 
   return (
     <Layout className="bg-ebsBlack">
       <div className="max-w-screen-md md:mx-auto mx-6 my-10">
         <h1>Photo Gallery</h1>
-        <div className="bg-white">
-          <Gallery photos={images} onClick={openLightbox} />
-          <ModalGateway>
-            {viewerIsOpen ? (
-              <Modal onClose={closeLightbox}>
-                <Carousel
-                  currentIndex={currentImage}
-                  views={images.map((x) => ({
-                    ...x,
-                    srcset: x.srcSet,
-                    caption: x.title,
-                  }))}
-                />
-              </Modal>
-            ) : null}
-          </ModalGateway>
+        <div className="bg-white p-[15px]">
+          <RowsPhotoAlbum
+            photos={photos}
+            targetRowHeight={150}
+            onClick={({ index }) => setCurrentImage(index)}
+          />
+          <Lightbox
+            index={currentImage}
+            open={currentImage >= 0}
+            slides={photos}
+            plugins={[Fullscreen, Zoom, Slideshow, Thumbnails]}
+            on={{
+              view: updateIndex,
+            }}
+            close={() => setCurrentImage(-1)}
+          />
         </div>
       </div>
     </Layout>
