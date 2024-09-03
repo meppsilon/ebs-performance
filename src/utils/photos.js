43f4dcdee82de9f957d610dd6a59f14e866c92bd
meppsilon <matthew@hyperbolic.xyz>
2024-09-03 09:42:47 -0700

    add photos

---
+++ src/utils/photos.js
@@ -0,0 +1,84 @@
+const photos = [
+  {
+    id: 'image-1',
+    src: '/img/carousel-1.jpg',
+    width: 720,
+    height: 900,
+  },
+  {
+    id: 'image-2',
+    src: '/img/carousel-2.jpg',
+    width: 2736,
+    height: 1824,
+  },
+  {
+    id: 'image-3',
+    src: '/img/carousel-3.jpg',
+    width: 2736,
+    height: 1824,
+  },
+  {
+    id: 'image-4',
+    src: '/img/carousel-4.jpg',
+    width: 2736,
+    height: 1824,
+  },
+  { id: 'image-5', src: '/img/carousel-5.jpg', width: 1824, height: 2736 },
+  { id: 'image-6', src: '/img/carousel-7.jpg', width: 1824, height: 2736 },
+  {
+    id: 'image-7',
+    src: '/img/carousel-7.jpg',
+    width: 724,
+    height: 1086,
+  },
+  {
+    id: 'image-8',
+    src: '/img/carousel-8.jpg',
+    width: 1824,
+    height: 2736,
+  },
+  {
+    id: 'image-9',
+    src: '/img/carousel-9.jpg',
+    width: 1824,
+    height: 2736,
+  },
+  {
+    id: 'image-10',
+    src: '/img/carousel-10.jpg',
+    width: 1824,
+    height: 2736,
+  },
+  {
+    id: 'image-11',
+    src: '/img/carousel-11.jpg',
+    width: 1824,
+    height: 2736,
+  },
+  {
+    id: 'image-12',
+    src: '/img/carousel-12.jpg',
+    width: 1814,
+    height: 2722,
+  },
+  {
+    id: 'image-13',
+    src: '/img/carousel-13.jpg',
+    width: 1824,
+    height: 2736,
+  },
+  {
+    id: 'image-14',
+    src: '/img/carousel-14.jpg',
+    width: 1824,
+    height: 2736,
+  },
+  {
+    id: 'image-15',
+    src: '/img/carousel-15.jpg',
+    width: 1824,
+    height: 2736,
+  },
+];
+
+export default photos;
